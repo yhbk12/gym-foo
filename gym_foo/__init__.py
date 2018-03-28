abeae18f5eecdf1b1ab8d3434702383095fce620
Yusuf Khaled <yusufkhaled@live.ca>
2018-03-28 10:56:12 -0400

    Update __init__.py

--- gym_foo/__init__.py
+++ gym_foo/__init__.py
@@ -3,6 +3,6 @@ from gym.envs.registration import register
 
 
 register(
-    id="wikinav-v0",
+    id="wikinav-v-yusuf",
     entry_point="gym_foo.envs:EmbeddingWikiNavEnv",
     timestep_limit=50)
