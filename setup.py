ccc3e70c99f8350b254d3fd804f4a128a7596177
Yusuf Khaled <yusufkhaled@live.ca>
2018-03-28 10:40:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup
 
-setup(name='gym_foo',
-      version='0.0.1',
-      install_requires=['gym>=0.2.3',
-                        'hfo_py>=0.2']
-      )
+setup(name="gym_wikinav",
+      version="0.0.1",
+      install_requires=["gym"])
